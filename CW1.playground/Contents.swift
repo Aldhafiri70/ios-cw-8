936de6a05f606f504568b8f9cb8ef9b4deb3d06c
Aldhafiri70 <thelordshoes@Gmail.com>
2022-03-12 11:04:22 +0300

    done

--- CW1.playground/Contents.swift
+++ CW1.playground/Contents.swift
@@ -1,5 +1,11 @@
-import UIKit
 
+import UIKit
+let name="Abdulrazaq"
+let birthyear=2007
+var age="14"
+var curyear=2022
+curyear - birthyear
+print("Hello my name is \(name) I am \(age) years old i was born in \(birthyear) ")
 // التمرين الأول
 // ClassWork 1
 
