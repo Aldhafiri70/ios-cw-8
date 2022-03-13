e3b9575d72bd4d462c40288af428ae7e1a04c8d4
Aldhafiri70 <thelordshoes@Gmail.com>
2022-03-13 21:34:56 +0300

    done

--- CW1.playground/Contents.swift
+++ CW1.playground/Contents.swift
@@ -6,6 +6,11 @@ var age="14"
 var curyear=2022
 curyear - birthyear
 print("Hello my name is \(name) I am \(age) years old i was born in \(birthyear) ")
+var num1="8"
+var num2 = 4
+var num3 = 2.5
+num3 = Double(num2) + (Double(num1) ?? 8)
+
 // التمرين الأول
 // ClassWork 1
 
